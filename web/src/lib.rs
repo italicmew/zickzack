803f2cd91f7ea7d09d49c386b023926da238f22d
italicmew <fabricio-bit@hotmail.com>
2023-10-26 19:00:20 -0300

    Added encrypt and decrypt fn

--- web/src/lib.rs
+++ web/src/lib.rs
@@ -1,3 +1,4 @@
+use aes_gcm::aead::generic_array::GenericArray;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 
@@ -10,8 +11,8 @@ use aes_gcm::{
 // Import the wasm-bindgen crate.
 use wasm_bindgen_futures::JsFuture;
 use web_sys::{
-    Document, Event, Headers, HtmlButtonElement, HtmlTextAreaElement, Location, Request,
-    RequestInit, RequestMode, Response, Window, HtmlElement
+    Document, Event, Headers, HtmlButtonElement, HtmlElement, HtmlTextAreaElement, Location,
+    Request, RequestInit, RequestMode, Response, Window,
 };
 
 #[wasm_bindgen]
@@ -20,11 +21,13 @@ pub fn show_alert() {
     let document: Document = window.document().expect("should have a document on window");
     if let Some(alert) = document.get_element_by_id("alerts") {
         if let Some(html_element) = alert.dyn_ref::<HtmlElement>() {
-            html_element.style().set_property("display", "block").unwrap();
+            html_element
+                .style()
+                .set_property("display", "block")
+                .unwrap();
         }
     }
 }
-
 
 #[wasm_bindgen]
 pub fn setup_submit_listener() -> Result<(), JsValue> {
@@ -94,47 +97,51 @@ pub fn setup_submit_listener() -> Result<(), JsValue> {
     Ok(())
 }
 
+fn decrypt_message(
+    key: &str,
+    nonce: &Vec<u8>,
+    ciphertext: &[u8],
+) -> Result<Vec<u8>, aes_gcm::aead::Error> {
+    if key.as_bytes().len() != 32 {
+        return Err(aes_gcm::aead::Error);
+    }
+
+    let cipher = Aes256Gcm::new(GenericArray::from_slice(key.as_bytes()));
+    let plaintext = cipher.decrypt(&GenericArray::from_slice(nonce), ciphertext);
+
+    plaintext
+}
+
+fn encrypt_message(key: &str, text: &str) -> Result<(Vec<u8>, Vec<u8>), aes_gcm::aead::Error> {
+    if key.as_bytes().len() != 32 {
+        return Err(aes_gcm::aead::Error);
+    }
+
+    let cipher = Aes256Gcm::new(GenericArray::from_slice(key.as_bytes()));
+    let nonce = Aes256Gcm::generate_nonce(&mut rand::rngs::OsRng).to_vec();
+    let ciphertext = cipher.encrypt(&GenericArray::from_slice(nonce.as_slice()), text.as_ref())?;
+
+    Ok((nonce, ciphertext))
+}
+
 #[wasm_bindgen(start)]
 async fn main() -> Result<(), JsValue> {
-    // Use `web_sys`'s global `window` function to get a handle on the global
-    // window object.
-
-    let key = Aes256Gcm::generate_key(OsRng);
-    web_sys::console::log_1(&JsValue::from_str(&format!("content=")));
-
-    let cipher = Aes256Gcm::new(&key);
-    let nonce = Aes256Gcm::generate_nonce(&mut OsRng); // 96-bits; unique per message
-    let ciphertext = cipher
-        .encrypt(&nonce, b"plaintext message".as_ref())
-        .unwrap();
-    let plaintext = cipher.decrypt(&nonce, ciphertext.as_ref()).unwrap();
-
-    let window = web_sys::window().expect("no global `window` exists");
-    let document = window.document().expect("should have a document on window");
-    let body = document.body().expect("document should have a body");
-
-    // Manufacture the element we're gonna append
-    let val = document.create_element("p")?;
-    val.set_inner_html(
-        format!(
-            "--encrypted text: {}",
-            std::str::from_utf8(&plaintext).unwrap()
-        )
-        .as_str(),
-    );
-
-    body.append_child(&val)?;
+    let w: Window =
+        web_sys::window().ok_or_else(|| JsValue::from_str("No global `window` exists"))?;
+    let document: Document = w
+        .document()
+        .ok_or_else(|| JsValue::from_str("No document available"))?;
 
     let text_area = document
         .get_element_by_id("editor")
         .expect("Element not found.");
 
-    let location: Location = window.location();
+    let location: Location = w.location();
     let current_path = location
         .pathname()
         .map_err(|_| JsValue::from_str("Failed to get pathname"))?;
     let editor_url = format!("/editor{}", current_path);
-    let promise = window.fetch_with_str(&editor_url);
+    let promise = w.fetch_with_str(&editor_url);
     let response: Response = JsFuture::from(promise).await?.dyn_into()?;
     let text: String = JsFuture::from(response.text()?)
         .await?
@@ -151,4 +158,24 @@ async fn main() -> Result<(), JsValue> {
 #[wasm_bindgen]
 pub fn add(a: i32, b: i32) -> i32 {
     return a + b;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(1, 2), 3);
+    }
+
+    #[test]
+    fn test_encrypt_message() {
+        let key = "12341234123412341234123412341234";
+        let text = "my test text";
+        let ret = encrypt_message(key, text).unwrap();
+        let decrypted = decrypt_message(key, &ret.0, &ret.1).unwrap();
+
+        assert_eq!(decrypted, text.as_bytes());
+    }
 }
