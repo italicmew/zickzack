2afcfba6de79c3bd16493f3cb5811b18e3fb598f
italicmew <fabricio-bit@hotmail.com>
2023-10-29 10:08:02 -0300

    Modify db to not include nonce as it will be appended

--- src/db.rs
+++ src/db.rs
@@ -1,12 +1,11 @@
-use rusqlite::{Connection, params};
-use serde_derive::{Serialize, Deserialize};
+use rusqlite::{params, Connection};
+use serde_derive::{Deserialize, Serialize};
 use sha2::{Digest, Sha256};
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Data {
     url: String,
     data: Option<Vec<u8>>,
-    nonce: Option<Vec<u8>>,
 }
 
 struct Database {
@@ -24,8 +23,7 @@ impl Database {
             "CREATE TABLE IF NOT EXISTS data (
                 id TEXT PRIMARY KEY,
                 url TEXT NOT NULL,
-                data BLOB,
-                nonce BLOB
+                data BLOB
             )",
             [],
         )
@@ -37,24 +35,28 @@ impl Database {
         let id = format!("{:x}", hasher.finalize());
 
         self.conn.execute(
-            "INSERT INTO data (id, url, data, nonce) VALUES (?1, ?2, ?3, ?4)",
-            params![id, data.url, data.data.as_deref(), data.nonce.as_deref()],
+            "INSERT INTO data (id, url, data) 
+            VALUES (?1, ?2, ?3)
+            ON CONFLICT(id) DO UPDATE SET
+                url = excluded.url,
+                data = excluded.data;",
+            params![id, data.url, data.data.as_deref()],
         )?;
         Ok(())
     }
 
     pub fn get_data(&self, url: String) -> Result<Option<Data>, rusqlite::Error> {
-
         let mut hasher = Sha256::new();
         hasher.update(&url);
         let id = format!("{:x}", hasher.finalize());
-        let mut stmt = self.conn.prepare("SELECT id, url, data, nonce FROM data WHERE id = ?1")?;
+        let mut stmt = self
+            .conn
+            .prepare("SELECT id, url, data FROM data WHERE id = ?1")?;
 
         let mut data_iter = stmt.query_map(params![id], |row| {
             Ok(Data {
                 url: row.get(1)?,
                 data: row.get(2)?,
-                nonce: row.get(3)?,
             })
         })?;
 
@@ -66,25 +68,29 @@ impl Database {
     }
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_add() {
+    fn test_database() {
         let db = Database::new(":memory:").unwrap();
         db.create_db().unwrap();
 
         let example_data = Data {
             url: "http://example.com".to_string(),
             data: Some(vec![1, 2, 3, 4, 5]),
-            nonce: Some(vec![5, 4, 3, 2, 1]),
         };
 
         db.save_data(&example_data).unwrap();
 
-        let result = db.get_data("http://example.com".to_string()).expect("Failed to retrieve data from database");
-        assert_eq!(result.map(|r| r.data), Some(example_data.data), "Retrieved data does not match saved data");
+        let result = db
+            .get_data("http://example.com".to_string())
+            .expect("Failed to retrieve data from database");
+        assert_eq!(
+            result.map(|r| r.data),
+            Some(example_data.data),
+            "Retrieved data does not match saved data"
+        );
     }
 }
